fbc9a393b688ccef7b861c1f401a3a28cfd2ea35
MaxTrgb <126002400+MaxTrgb@users.noreply.github.com>
2023-12-21 01:14:46 +0200

    Add files via upload

---
+++ Refactoring/C#CLASS25.11.2/C#CLASS25.11.2/ChoseHeroMenu.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace C_CLASS25._11._2
+{
+    internal class ChoseHeroMenu
+    {
+        ChooseHero cHero = new ChooseHero();
+        FightMain fightMain = new FightMain();
+        private int totalDamage = 0;
+        private int totalDamage2 = 0;
+        private bool isPlayer1Turn = true;
+
+        public void chooseHeroMenu()
+        {
+            while (true)
+            {
+                
+                ConsoleKeyInfo loc;
+
+                Console.Clear();
+                DateTime date;
+                date = DateTime.Now;
+                Statistics statistics = new Statistics(date.ToLongDateString());
+
+                string currentPlayer = isPlayer1Turn ? "Player one" : "Player two";
+
+                Hero Player1 = cHero.chooseHero("Player one");
+                Console.Clear();
+                Hero Player2 = cHero.chooseHero("Player two");
+                Console.Clear();
+
+                statistics.classPlayerOne = Player1.Name;
+                statistics.classPlayerTwo = Player2.Name;
+
+                Console.WriteLine($"\n\t\t\tChoose location:");
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine($"\n\t\t\t1.Arena");
+                Console.ResetColor();
+
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                Console.WriteLine($"\n\t\t\t2.Everest");
+                Console.ResetColor();
+
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine($"\n\t\t\t3.Forest");
+                Console.ResetColor();
+
+                loc = Console.ReadKey();
+
+                if (loc.KeyChar == '1')
+                {
+                    Player1.Location(Hero.BattleLocation.Arena);
+                    Player2.Location(Hero.BattleLocation.Arena);
+                }
+                else if (loc.KeyChar == '2')
+                {
+                    Player1.Location(Hero.BattleLocation.Everest);
+                    Player2.Location(Hero.BattleLocation.Everest);
+                }
+                else if (loc.KeyChar == '3')
+                {
+                    Player1.Location(Hero.BattleLocation.Forest);
+                    Player2.Location(Hero.BattleLocation.Forest);
+                }
+                Console.Clear();
+
+                Console.WriteLine($"\n\nPress enter to start the game!");
+                Console.ReadLine();
+                Console.Clear();
+                
+                
+                fightMain.runFight(totalDamage, totalDamage2, isPlayer1Turn, Player1, Player2, currentPlayer, statistics, date);
+                           
+                break;
+            }
+        }
+
+    }
+}
---
+++ Refactoring/C#CLASS25.11.2/C#CLASS25.11.2/FightMain.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace C_CLASS25._11._2
+{
+    
+    internal class FightMain
+    {
+       
+        private int skillCounterPOne = 0;
+        private int skillCounterPTwo = 0;
+        private ConsoleKeyInfo move1;
+        private ConsoleKeyInfo move2;
+
+        public void runFight(int totalDamage, int totalDamage2, bool isPlayer1Turn, Hero Player1, Hero Player2, string currentPlayer, Statistics statistics, DateTime date)
+        {
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine($"\nTotal damage Player one: {totalDamage} ");
+                Console.WriteLine($"Total damage Player two: {totalDamage2} ");
+                Console.ResetColor();
+
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine($"Player one remaining health: {Player1.Health}");
+                Console.WriteLine($"Player two remaining health: {Player2.Health}");
+                Console.ResetColor();
+
+                Console.WriteLine($"\n\t\t\t{currentPlayer} move:");
+                Console.WriteLine("\t\t\t1.Attack\n\t\t\t2.Defence\n\t\t\t3.Heal\n\t\t\t4.Skill");
+                move1 = Console.ReadKey();
+                Console.WriteLine();
+
+                int damage = 0;
+                int attckTypeChoice = 0;
+
+
+                switch (move1.KeyChar)
+                {
+                    case '1':
+
+                        damage = Player1.CalculateDamage(Player2.AttackPower, Hero.AttackType.Physical, Player1.CriticalChance, Player1.Name, move1);
+                        damage = Player2.Defend(damage);
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.WriteLine($"\n{currentPlayer} dealt {damage} damage.");
+                        Console.ResetColor();
+
+                        break;
+                    case '2':
+                        Player1.StartDefending();
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine($"{currentPlayer} started defending.");
+                        Console.ResetColor();
+
+                        break;
+                    case '3':
+                        Player1.Heal();
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"{currentPlayer} healed.");
+                        Console.ResetColor();
+
+                        break;
+                    case '4':
+                        if (skillCounterPOne == 0)
+                        {
+                            damage = Player1.CalculateDamage(Player2.AttackPower, Hero.AttackType.Physical, Player1.CriticalChance, Player1.Name, move1);
+                            damage = Player1.Skill(damage);
+                            damage = Player2.Defend(damage);
+
+                            Console.ForegroundColor = ConsoleColor.DarkYellow;
+                            Console.WriteLine($"{currentPlayer} dealt {damage} damage.");
+                            Console.ResetColor();
+                            skillCounterPOne = 3;
+
+                        }
+                        else
+                        {
+
+                            if (skillCounterPOne == 1)
+                            {
+                                Console.WriteLine($"Skill is on the cooldown!\n{skillCounterPOne} move left...");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Skill is on the cooldown!\n{skillCounterPOne} moves left...");
+
+                            }
+
+                        }
+                        break;
+
+                }
+                totalDamage += damage;
+
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine($"\nPlayer one remaining health: {Player1.Health}");
+                Console.WriteLine($"Player two remaining health: {Player2.Health}");
+                Console.ResetColor();
+
+                if (Player1.Health <= 0 || Player2.Health <= 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine($"\t\t\t{currentPlayer} wins!");
+                    Console.ResetColor();
+
+                    statistics.Winner = currentPlayer;
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine($"Total damage Player one: {totalDamage} ");
+                    Console.WriteLine($"Total damage Player two: {totalDamage2} ");
+                    Console.ResetColor();
+
+                    Console.WriteLine("\n\nPress enter to return to the main menu...");
+                    Console.ReadLine();
+                    break;
+                }
+                Console.WriteLine("\n\nPress enter to continue...");
+                Console.ReadLine();
+                Console.Clear();
+                currentPlayer = isPlayer1Turn ? "Player two" : "Player one";
+
+                // Player 2's turn
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine($"\nTotal damage Player one: {totalDamage} ");
+                Console.WriteLine($"Total damage Player two: {totalDamage2} ");
+                Console.ResetColor();
+
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine($"Player one remaining health: {Player1.Health}");
+                Console.WriteLine($"Player two remaining health: {Player2.Health}");
+                Console.ResetColor();
+
+                Console.WriteLine($"\n\t\t\t{currentPlayer} move:");
+                Console.WriteLine("\t\t\t1.Attack\n\t\t\t2.Defence\n\t\t\t3.Heal\n\t\t\t4.Skill");
+                move2 = Console.ReadKey();
+                Console.WriteLine();
+
+                int damage2 = 0;
+
+
+                switch (move2.KeyChar)
+                {
+                    case '1':
+                        damage2 = Player2.CalculateDamage(Player1.AttackPower, Hero.AttackType.Physical, Player2.CriticalChance, Player2.Name, move2);
+                        damage2 = Player1.Defend(damage2);
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.WriteLine($"\n{currentPlayer} dealt {damage2} damage.");
+                        Console.ResetColor();
+
+                        break;
+                    case '2':
+                        Player2.StartDefending();
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine($"{currentPlayer} started defending.");
+                        Console.ResetColor();
+
+                        break;
+                    case '3':
+                        Player2.Heal();
+                        Console.WriteLine($"{currentPlayer} healed.");
+                        break;
+                    case '4':
+                        if (skillCounterPTwo == 0)
+                        {
+                            damage2 = Player2.CalculateDamage(Player1.AttackPower, Hero.AttackType.Physical, Player2.CriticalChance, Player2.Name, move2);
+                            damage2 = Player2.Skill(damage2);
+                            damage2 = Player1.Defend(damage2);
+                            Console.ForegroundColor = ConsoleColor.DarkYellow;
+                            Console.WriteLine($"{currentPlayer} dealt {damage2} damage.");
+                            Console.ResetColor();
+                            skillCounterPTwo = 3;
+                        }
+                        else
+                        {
+                            if (skillCounterPTwo == 1)
+                            {
+                                Console.WriteLine($"\t\t\tSkill is on the cooldown!\n{skillCounterPTwo} move left...");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"\t\t\tSkill is on the cooldown!\n{skillCounterPTwo} moves left...");
+
+                            }
+                        }
+                        break;
+                }
+                totalDamage2 += damage2;
+
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine($"\nPlayer one remaining health: {Player1.Health}");
+                Console.WriteLine($"Player two remaining health: {Player2.Health}");
+                Console.ResetColor();
+
+
+                if (Player1.Health <= 0 || Player2.Health <= 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine($"\t\t\t{currentPlayer} wins!");
+                    Console.ResetColor();
+                    statistics.Winner = currentPlayer;
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine($"Total damage Player one: {totalDamage} ");
+                    Console.WriteLine($"Total damage Player two: {totalDamage2} ");
+                    Console.ResetColor();
+                    Console.WriteLine("\nPress enter to return to the main menu...");
+                    Console.ReadLine();
+
+                    break;
+                }
+                Console.WriteLine("\nPress enter to continue...");
+                Console.ReadLine();
+                Console.Clear();
+                currentPlayer = isPlayer1Turn ? "Player one" : "Player two";
+
+                if (skillCounterPOne > 0)
+                {
+                    skillCounterPOne--;
+                }
+                if (skillCounterPTwo > 0)
+                {
+                    skillCounterPTwo--;
+                }
+                statistics.totalDamagePlayerOne = totalDamage;
+                statistics.totalDamagePlayerTwo = totalDamage2;
+                string path = date.ToLongTimeString();
+                path = path.Replace(":", "-");
+                using (FileStream? file = new FileStream(@$"..\..\{path}.json", FileMode.Create))
+                {
+                    JsonSerializer.Serialize<Statistics>(file, statistics);
+                    Console.WriteLine(file.Name);
+                }
+
+            }
+        }
+    }
+}
